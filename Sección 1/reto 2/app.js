723fe056a7560af898234979fb7be4d7f08fa6ae
Jerónimo <jerocruz76@gmail.com>
2024-04-17 16:15:11 -0500

    add 2nd challenge

--- Sección 1/reto 2/app.js
+++ Sección 1/reto 2/app.js
@@ -1,29 +1,15 @@
-let listSouvenirs = {
-    nombre:nombreS,
-    disponibility:disponibilidad,
-    price:precio}
-console.log(listSouvenirs)
+let souvernir = prompt("Ingresa el nombre de los souvernirs separados por coma")
+listSouvenir = souvernir.split(",")
+let listPrice = []
+let listDispo = []
+
+listSouvenir.forEach(function(i) {
+    let price = parseFloat(prompt(`ingresa el valor del souvenir ${i}`))
+    listPrice.push(price)
+    confirmar = confirm("Está disponible?")
+    listDispo.push(confirmar)     
+});
 
 
 
-confirmar = confirm("Deseas agregar un souvenir?")
-
-while (confirmar = true) {
-    nombreS = prompt("Ingresa el nombre del souvenir")
-    disponibilidad = confirm("Está disponible?") || false
-    precio = prompt(parseFloat("Cuanto cuesta?"))
-
-    if (typeof nombre === 'string' && typeof disponibilidad === 'boolean' && !isNaN(precio)) {
-        listSouvenirs[nombre] = {nombreS}
-        listSouvenirs[disponibility] = {disponibilidad}
-        listSouvenirs[price] = {precio}
-        alert("Souvenir guardado")
-        console.table(listSouvenirs)
-    }
-
-    else { alert("Ingresa un valor correcto") } break
-    
-}
-
-alert("Ésta es tu lista de souvenirs" + JSON.stringify(listSouvenirs))
 
